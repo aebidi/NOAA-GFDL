ff9177f7ee36b7f10fbb32bfd4abff953d80f292
Abdullah <abdullah.mubasshar@syvl.earth>
2025-09-23 09:45:43 +0000

    expanded search parameters to download data from other ensemble members

--- README.md
+++
@@ -1,2 +0,0 @@
-# NOAA-GFDL
-Repository for the NOAA GFDL (Geophysical Fluid Dynamics Laboratory) data acquisition pipeline.
--- main.py
+++ main.py
@@ -38,114 +38,60 @@ def main():
     for dataset in config['datasets']:
         logging.info(f"\n===== Starting Dataset: {dataset['name']} =====")
         
-        for variable in dataset.get('variables_to_download', []):
-            for mip_table in dataset['mip_tables_to_try']:
-                for grid in dataset['grids_to_try']:
-                    for member in dataset['ensemble_members']:
-                        # iterate through the list of time period objects
-                        for period_info in dataset['time_periods']:
-                            
-                            # extracting the period_string for filename construction
-                            period_string = period_info['period_string']
-                            
-                            url = dataset['url_template'].format(
-                                experiment=dataset['experiment'],
-                                ensemble_member=member,
-                                mip_table=mip_table,
-                                variable=variable,
-                                grid=grid,
-                                time_period=period_string
-                            )
-                            file_name = url.split('/')[-1]
+        # get the global discovery lists
+        ensemble_members = dataset.get('ensemble_members', [])
+        grids_to_try = dataset.get('grids_to_try', [])
+        versions_to_try = dataset.get('versions_to_try', [])
 
-                            # --- DOWNLOAD STAGE (only once per file) ---
-                            raw_file_path = download_file(url, raw_download_dir)
+        # loop through each defined configuration group (e.g., monthly, daily)
+        for group in dataset.get('configuration_groups', []):
+            logging.info(f"--> Searching in Group: {group['name']}")
+            
+            mip_tables = group.get('mip_tables', [])
+            time_periods = group.get('time_periods', [])
+            variables = group.get('variables', [])
 
-                            # --- MULTI-REGION PROCESSING STAGE ---
-                            if raw_file_path:
-                                # loop through each defined region
-                                for region in processing_regions:
-                                    region_name = region['name']
-                                    geo_scope = region['bounding_box']
+            # --- full discovery nested loops ---
+            for variable in variables:
+                for mip_table in mip_tables:
+                    for member in ensemble_members:
+                        for grid in grids_to_try:
+                            for version in versions_to_try:
+                                for period in time_periods:
                                     
-                                    logging.info(f"-> Processing '{file_name}' for region: {region_name}")
-
-                                    # constructing the region-specific final path
-                                    category = get_variable_category(variable, variable_map)
-                                    final_processed_dir = os.path.join(
-                                        base_path, 
-                                        region_name, # subdirectory for the region
-                                        dataset['model'], 
-                                        dataset['experiment'], 
-                                        category
+                                    url = dataset['url_template'].format(
+                                        experiment=dataset['experiment'],
+                                        ensemble_member=member,
+                                        mip_table=mip_table,
+                                        variable=variable,
+                                        grid=grid,
+                                        version=version,
+                                        time_period=period
                                     )
-                                    ensure_dir_exists(final_processed_dir)
-                                    
-                                    processed_file_path = os.path.join(final_processed_dir, file_name)
-                                    
-                                    # processing the single raw file for the current region
-                                    process_netcdf_file(raw_file_path, processed_file_path, geo_scope)
+                                    file_name = url.split('/')[-1]
+
+                                    # --- DOWNLOAD STAGE ---
+                                    raw_file_path = download_file(url, raw_download_dir)
+
+                                    # --- MULTI-REGION PROCESSING STAGE ---
+                                    if raw_file_path:
+                                        for region in processing_regions:
+                                            region_name = region['name']
+                                            geo_scope = region['bounding_box']
+                                            
+                                            category = get_variable_category(variable, variable_map)
+                                            final_processed_dir = os.path.join(
+                                                base_path, region_name, dataset['model'], 
+                                                dataset['experiment'], category
+                                            )
+                                            ensure_dir_exists(final_processed_dir)
+                                            
+                                            processed_file_path = os.path.join(final_processed_dir, file_name)
+                                            
+                                            process_netcdf_file(raw_file_path, processed_file_path, geo_scope)
 
     logging.info("\n--- GFDL Data Pipeline Finished ---")
 
 
-'''
-def main():
-    """Main pipeline orchestrator"""
-    with open("config.yaml", 'r') as f:
-        config = yaml.safe_load(f)
-
-    base_path = config['base_data_path']
-    log_path = os.path.join(base_path, config['log_file'])
-    ensure_dir_exists(base_path)
-    logger = setup_logging(log_path)
-    logger.info("--- GFDL Data Pipeline Started ---")
-
-    check_storage(base_path, required_gb=50)
-    geo_scope = config['geographical_scope']
-    variable_map = config['variable_categories']
-    
-    raw_download_dir = os.path.join(base_path, config['raw_data_dir'])
-    ensure_dir_exists(raw_download_dir)
-
-    for dataset in config['datasets']:
-        logger.info(f"Starting dataset: {dataset['name']}")
-        
-        for variable in dataset['variables_to_download']:
-            category = get_variable_category(variable, variable_map)
-            final_processed_dir = os.path.join(
-                base_path, 
-                dataset['model'], 
-                dataset['experiment'], 
-                category
-            )
-            ensure_dir_exists(final_processed_dir)
-
-            for mip_table in dataset['mip_tables_to_try']:
-                # final nested loop to try all grids
-                for grid in dataset['grids_to_try']:
-                    logger.debug(f"Searching for '{variable}' (MIP: {mip_table}, Grid: {grid})")
-                    
-                    for member in dataset['ensemble_members']:
-                        for period in dataset['time_periods']:
-                            # construct the URL with all placeholders
-                            url = dataset['url_template'].format(
-                                experiment=dataset['experiment'],
-                                ensemble_member=member,
-                                mip_table=mip_table,
-                                variable=variable,
-                                grid=grid, 
-                                time_period=period
-                            )
-
-                            raw_file_path = download_file(url, raw_download_dir)
-                            
-                            if raw_file_path:
-                                file_name = os.path.basename(raw_file_path)
-                                processed_file_path = os.path.join(final_processed_dir, file_name)
-                                process_netcdf_file(raw_file_path, processed_file_path, geo_scope)
-
-    logger.info("--- GFDL Data Pipeline Finished ---")
-'''
 if __name__ == "__main__":
     main()
